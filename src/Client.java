cda8d9eb1742fb82a3946d780117c4c32388e3ad
daskelito <henrik.heinze@hotmail.com>
2021-10-06 12:23:01 +0200

    514

--- src/Client.java
+++ src/Client.java
@@ -1,47 +1,82 @@
-import javax.xml.crypto.Data;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
+import java.util.Date;
 
-public class Client {
-    private int port;
-    private Socket socket;
-    DataInputStream dis;
-    DataOutputStream dos;
 
-//    public Client(int port) throws IOException {
-//        this.port = port;
-//        socket = new Socket("localhost", port);
-//        dis = new DataInputStream(socket.getInputStream());
-//        dos = new DataOutputStream(socket.getOutputStream());
-//    }
+public class Client implements Serializable {
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private final String ip;
+    private final int port;
 
-    public Client(int port) {
-        try (Socket socket = new Socket("localhost", port)) {
-            dis = new DataInputStream(socket.getInputStream());
-            dos = new DataOutputStream(socket.getOutputStream());
+
+    public Client(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+        new Connection().start();
+    }
+
+
+    public void send(String message) {
+        try {
+            dos.write(message.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
-            System.err.println(e);
+            e.printStackTrace();
         }
     }
 
-    public void send(String message) throws IOException {
-        dos.write(message.getBytes(StandardCharsets.UTF_8));
-        dos.flush();
+    /**
+     * disconnects the client from the server and closing the streams.
+     */
+    public void disconnect() {
+        try {
+            dos.flush();
+            dos.close();
+            dis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private class Listener extends Thread {
+    /**
+     * the Connection class is establishing and maintaining the connection to the server.
+     * Starting a listener for incoming data from the server.
+     */
+    private class Connection extends Thread {
+        @Override
         public void run() {
-            String response;
             try {
-                while (true) {
-                    response = dis.readUTF();
+                Socket socket = new Socket(ip, port);
+                dos = new DataOutputStream(socket.getOutputStream());
+                dis = new DataInputStream(socket.getInputStream());
+                new listener().start();
+
+                while (!Thread.interrupted()) {
+
                 }
             } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
+    }
 
+    /**
+     * listener listens to all incoming data from the server.
+     */
+    private class listener extends Thread {
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                send("afsafsfasfas");
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 public class main {
     public static void main(String[] args) throws IOException {
         WackServer ws = new  WackServer(888);
-        Client client = new Client(888);
-        client.send("afsafsfasfas");
+        Client client = new Client("localhost", 888);
     }
 }
